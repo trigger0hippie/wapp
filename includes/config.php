17d8b2e07d0dcf4a1af627f985e290990835930f
trigger0hippie <trigger.hippie@email.t-com.hr>
2017-03-20 14:50:13 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,8 +3,8 @@
 	define("TITLE", "Film-O-Matik");
 	define("UPLOAD_PATH", "storage/");
 	define("XML_SCHEMA", "storage/xmlschema.xsd");
-	$db = mysqli_connect("master.doma", "root", "Hacker1981", "WEBAPP");
-	$db2 = mysqli_connect("master.doma", "root", "Hacker1981", "MyVideos93");
+	$db = mysqli_connect("master.doma", "user", "pass", "WEBAPP");
+	$db2 = mysqli_connect("master.doma", "user", "pass", "MyVideos");
 	if (!$db) 
 	{
 		die("Connection error: " . mysqli_connect_errno());
